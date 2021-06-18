8eca4d6cb28eccb1eaeafc8b7012f339ea94fbf9
Adrian Li <li.adrianmc@gmail.com>
2021-06-17 21:20:23 -0400

    try code w/ serverless

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,37 @@
+require("dotenv").config();
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { BigNumber, ethers, Contract } from "ethers";
+import abi from "./abi.json";
 
-// request -> ?id={your_id}
+const DFX = {
+  address: "0x888888435fde8e7d4c54cab67f206e4199454c60",
+  decimals: 18,
+};
+
+const EXCLUDE = [
+  "0xa4fc358455febe425536fd1878be67ffdbdec59a", // sablier vesting funds
+  "0x27e843260c71443b4cc8cb6bf226c3f77b9695af", // governance multisig
+  "0x26f539a0fe189a7f228d7982bf10bc294fa9070c", // treasury multisig
+];
+
+// calculate excluded amounts, add them up, and subtract from total supply
+const calcCircSupply = async (dfx: Contract): Promise<BigNumber> => {
+  const exclusions: BigNumber[] = await Promise.all(
+    EXCLUDE.map((addr) => dfx.balanceOf(addr)),
+  );
+  const totalExclusion = exclusions.reduce((acc, x) => acc.add(x));
+  const totalSupply: BigNumber = await dfx.totalSupply();
+
+  return totalSupply.sub(totalExclusion);
+};
+
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const { query } = req;
-  const { id } = query;
-  res.json({ id });
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const dfx = new Contract(DFX.address, abi, provider);
+
+  const rawCircSupply: BigNumber = await calcCircSupply(dfx);
+  const circSupply = ethers.utils.formatUnits(rawCircSupply, DFX.decimals);
+
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send(circSupply.toString());
 };
